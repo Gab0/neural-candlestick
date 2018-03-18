7281ce418ec95e01eae097cef007201a95ec2d75
Gab0 <gabriel_scf@hotmail.com>
2018-03-18 12:39:01 -0300

    server progress, WIP and DNW

--- httpInterface.py
+++ httpInterface.py
@@ -9,9 +9,9 @@ stdHandler = http.server.SimpleHTTPRequestHandler
 class getHandler(http.server.SimpleHTTPRequestHandler):
     def do_GET(self):
 
-        print(self.send_head().read())
         param=urllib.parse.parse_qs(urllib.parse.urlparse(self.path).query)
         print(param)
+
 def getServer(handler=getHandler, port=2999):
     httpd = socketserver.TCPServer(("", port), handler)
     server = Thread(target=httpd.serve_forever)
