f5d992454461452ddfe019b825ed5383a7c7cfcb
Gab0 <gabriel_scf@hotmail.com>
2018-03-20 18:31:01 -0300

    the ensemble is still useless

--- neural-candlestick.py
+++ neural-candlestick.py
@@ -2,11 +2,32 @@
 
 
 import httpInterface
+import neuralGenesis
+import subprocess
+import sys
+import time
 
-port = 2997
+port = int(sys.argv[-1])
 addr = ('', port)
 
-Server = httpInterface.getServer(port=port)
+
+def process(self, candle):
+    print("processing %s" % candle)
+    return candle
 
 
-print("neural-candles server running on %s" % port)
+
+
+
+
+def testServer(port):
+    Q = subprocess.Popen([ 'wget', 'http://127.0.0.1:%i?candle=2333' % port, '--spider' ], stdout=subprocess.PIPE)
+    print("WGET TEST TEXT:")
+    time.sleep(2)
+    print(Q.stdout.read())
+
+    print("neural-candles server running on %s" % port)
+
+
+Server = httpInterface.getServer(responseFunction=process, port=port)
+Network = neuralGenesis.getNetwork()
